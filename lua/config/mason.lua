5550ac67109afbe4d7540fd33591ad87c8a7196e
Thomas F. Varley <contact@thosvarley.me>
2025-05-09 15:07:05 -0400

    Updating mason

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -14,11 +14,11 @@ require("mason-lspconfig").setup({
     		'r_language_server', 
 	}
 })
-
+--[[
 require("mason-lspconfig").setup_handlers {
   function(server_name)
     require("lspconfig")[server_name].setup {
       capabilities = require("cmp_nvim_lsp").default_capabilities()
     }
   end,
-}
+}]]--
