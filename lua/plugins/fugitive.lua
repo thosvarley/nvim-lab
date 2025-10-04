fef37002753af0ed5a99c026aebf304768e0df3f
Thomas F. Varley <contact@thosvarley.me>
2025-10-04 16:45:07 -0400

    Fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,7 @@
 return {
-	{ "tpope/vim-fugitive" },
+	{
+		"tpope/vim-fugitive",
+		cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit", "Gwrite", "Gread", "Gedit", "Gstatus", "Gcommit", "Gpush", "Gpull", "Gblame", "Glog" },
+		event = "VeryLazy", -- Optional: lazy load on startup
+	},
 }
