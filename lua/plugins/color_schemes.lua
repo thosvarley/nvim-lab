e4e68395e37844475041e23eac8a93463f903667
Thomas F. Varley <contact@thosvarley.me>
2025-10-06 17:09:18 -0400

    Updating color schemes

---
+++ lua/plugins/color_schemes.lua
@@ -0,0 +1,15 @@
+return {
+	"thesimonho/kanagawa-paper.nvim",
+	lazy = false,
+	priority = 1000,
+	init = function()
+		vim.cmd.colorscheme("kanagawa-paper-ink")
+	end,
+	integrations = {
+		wezterm = {
+			enabled = true,
+			path = (os.getenv("TEMP") or "/tmp") .. "/nvim-theme"
+		},
+	},
+	opts = { ... },
+}
